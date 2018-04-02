bab9e84111089a2c3b6eadbf0e18b1564b5f8931
johannajaimes <38020281+johannajaimes@users.noreply.github.com>
2018-04-02 18:42:58 -0500

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -1,7 +1,7 @@
 #initialize browser
 $ie = New-Object -com internetexplorer.application;
 $ie.visible = $true;
-$ie.navigate($loginUrl);
+$ie.navigate("http://lmnapp75/BPMDDILP/");
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
 ($ie.document.getElementsByName("menuQuery") |select -first 1).value = "DD-4266-CV";
