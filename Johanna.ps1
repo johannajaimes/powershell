3f4116db1a1fe894c95f1ed4a55b6fa30670f711
johannajaimes <38020281+johannajaimes@users.noreply.github.com>
2018-04-02 18:31:38 -0500

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -1,1 +1,12 @@
+#initialize browser
+$ie = New-Object -com internetexplorer.application;
+$ie.visible = $true;
+$ie.navigate($loginUrl);
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
+
+#login
+($ie.document.getElementsByName("username") |select -first 1).value = $username;
+($ie.document.getElementsByName("password") |select -first 1).value = $password;
+($ie.document.getElementsByName("login") |select -first 1).click();
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
