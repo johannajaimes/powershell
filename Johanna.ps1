d7a899e3959cc1b0ee0506187e27886a2a3a1e18
fragamus <innovative.engineer@gmail.com>
2018-04-02 16:41:16 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -4,8 +4,6 @@ $ie.visible = $true;
 $ie.navigate($loginUrl);
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
-#login
 ($ie.document.getElementsByName("menuQuery") |select -first 1).value = "DD-4266-CV";
-($ie.document.getElementsByName("blahblahblah") |select -first 1).click();
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
