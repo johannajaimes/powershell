a884d2b51665e422e75ea9a6ec0812812935c713
fragamus <innovative.engineer@gmail.com>
2018-04-02 18:12:16 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -1,8 +1,8 @@
 #initialize browser
 $ie = New-Object -com internetexplorer.application;
 $ie.visible = $true;
-$ie.navigate("http://lmnapp75/BPMDDILP/");
-while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
+#$ie.navigate("http://lmnapp75/BPMDDILP/");
+#while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $ie.document.getElementById("menuQuery").value = "DD-4266-CV";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
