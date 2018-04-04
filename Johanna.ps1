93ab7fd597bf67eec19d794b8963508ec9b1d2b4
fragamus <innovative.engineer@gmail.com>
2018-04-04 15:52:32 -0700

    use getElementsByName

--- Johanna.ps1
+++ Johanna.ps1
@@ -8,19 +8,7 @@ while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $counter = 0
 while (($counter -lt 100) -and ($ie.document -eq $null)) {Start-Sleep 1; $counter++}
 
-
-
-If ($ie) {'$ie is nice'
-If ($ie.document) {'$ie.document is nice'
-If ($ie.document.getElementById("menuQuery")) {'$ie.document.getElementById("menuQuery") is nice'
-If ($ie.document.getElementById("menuQuery").value) {'$ie.document.getElementById("menuQuery").value is nice'
-}  Else {'$ie.document.getElementById("menuQuery").value IS NOT NICE!'} 
-}  Else {'$ie.document.getElementById("menuQuery") IS NOT NICE!'} 
-}  Else {'$ie.document IS NOT NICE!'} 
-}  Else {'$ie IS NOT NICE!'} 
-
-$ie.document.getElementById("menuQuery").value;
-$ie.document.getElementById("menuQuery").value = "DD-4266-CV";
+($ie.document.getElementsByName("menuQuery")|select -first 1).value = "DD-4266-CV";
 
 #$ie.document.getElementById("lst-ib").value = "sugar puppy";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
