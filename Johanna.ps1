3c140f3284d51133d221d027eebabc66ee88e5f2
fragamus <innovative.engineer@gmail.com>
2018-04-03 18:25:04 -0700

    nice and not nice

--- Johanna.ps1
+++ Johanna.ps1
@@ -4,7 +4,18 @@ $ie.visible = $true;
 #$ie.navigate("http://lmnapp75/BPMDDILP/");
 $ie.navigate("http://www.google.com");
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
-#$ie.document.getElementById("menuQuery").value = "DD-4266-CV";
-$ie.document.getElementById("lst-ib").value = "sugar puppy";
+
+If ($ie) {'$ie is nice'
+If ($ie.document) {'$ie.document is nice'
+If ($ie.document.getElementById("menuQuery")) {'$ie.document.getElementById("menuQuery") is nice'
+If ($ie.document.getElementById("menuQuery").value) {'$ie.document.getElementById("menuQuery").value is nice'
+}  Else {'$ie.document.getElementById("menuQuery").value IS NOT NICE!'} 
+}  Else {'$ie.document.getElementById("menuQuery") IS NOT NICE!'} 
+}  Else {'$ie.document IS NOT NICE!'} 
+}  Else {'$ie IS NOT NICE!'} 
+
+$ie.document.getElementById("menuQuery").value = "DD-4266-CV";
+
+#$ie.document.getElementById("lst-ib").value = "sugar puppy";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
