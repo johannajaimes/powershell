1b30e3868845573316a2052799b07e6394a747c6
fragamus <innovative.engineer@gmail.com>
2018-04-04 23:12:23 -0700

    find the contentWindow(s)

--- Johanna.ps1
+++ Johanna.ps1
@@ -8,6 +8,17 @@ while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $counter = 0
 while (($counter -lt 100) -and ($ie.document -eq $null)) {Start-Sleep 1; $counter++}
 
+$es = $ie.document.getElementsByTagName("*")
+$n = $es.length;
+for ($i=0;$i -lt $n; $i++) {
+
+    if ($es[$i].hasAttribute("contentWindow"))
+    {
+        "The following elementId has a contentWindow: "
+        $es[$i].id
+    }
+}
+
 ($ie.document.getElementsByName("menuQuery")|select -first 1).value = "DD-4266-CV";
 
 #$ie.document.getElementById("lst-ib").value = "sugar puppy";
