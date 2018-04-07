db3bfe238cdb81a668a173ac1890641243c72cf0
fragamus <innovative.engineer@gmail.com>
2018-04-06 17:15:14 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -5,7 +5,7 @@ $ie.navigate("http://lmnapp75/BPMDDILP/");
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $counter = 0;
 while (($counter -lt 100) -and ($ie.document -eq $null)) {Start-Sleep 1; $counter++}
-($ie.document.getElementsByName("menuQuery")|select -first 1).click();
-($ie.document.getElementsByName("menuQuery")|select -first 1).value = "DD-4266-CV";
+$ie.document.getElementById("menuQuery").click();
+$ie.document.getElementById("menuQuery").value = "DD-4266-CV";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
