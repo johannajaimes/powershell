9c009a6b081bd4490e16f6b685ebc31fdc8266bf
fragamus <innovative.engineer@gmail.com>
2018-04-07 11:47:25 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -14,6 +14,7 @@ If ($ie.Document.getElementById("menuQuery").value) {'$ie.Document.getElementByI
 }  Else {'$ie IS NOT NICE!'}
 $ie.Document.getElementById("menuQuery");
 $ie.Document.getElementById("menuQuery").click();
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $ie.Document.getElementById("menuQuery").value = "DD-4266-CV";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
