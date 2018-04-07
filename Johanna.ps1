db8c54d7c6ef5e5af42ab00ecc83f509c58e99ef
fragamus <innovative.engineer@gmail.com>
2018-04-07 14:04:11 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -13,6 +13,14 @@ If ($ie.Document.getElementById("menuQuery").value) {'$ie.Document.getElementByI
 }  Else {'$ie.Document IS NOT NICE!'} 
 }  Else {'$ie IS NOT NICE!'}
 
+
+$frames = $ie.document.getElementsByTagName("frame") 
+Foreach($element in $frames )
+{
+    'found a frame';
+    $element.id;
+}
+
 $Doc = $ie.Document
 $menuQuery = $Doc.getElementById('menuQuery')
 $menuQuery.value = "DD-4266-CV"
