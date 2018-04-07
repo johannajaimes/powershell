c024773aa4a56223483ed35dbcab7ef977d2df49
fragamus <innovative.engineer@gmail.com>
2018-04-07 11:40:38 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -4,7 +4,15 @@ $ie.visible = $true;
 #$ie.navigate("http://lmnapp75/BPMDDILP/");
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $counter = 0;
-while (($counter -lt 100) -and ($ie.document -eq $null)) {Start-Sleep 1; $counter++}
+while (($counter -lt 100) -and ($ie.Document -eq $null)) {Start-Sleep 1; $counter++}
+If ($ie) {'$ie is nice'
+If ($ie.Document) {'$ie.Document is nice'
+If ($ie.Document.getElementById("menuQuery")) {'$ie.Document.getElementById("menuQuery") is nice'
+If ($ie.Document.getElementById("menuQuery").value) {'$ie.Document.getElementById("menuQuery").value is nice'
+}  Else {'$ie.Document.getElementById("menuQuery").value IS NOT NICE!'} 
+}  Else {'$ie.Document.getElementById("menuQuery") IS NOT NICE!'} 
+}  Else {'$ie.Document IS NOT NICE!'} 
+}  Else {'$ie IS NOT NICE!'}
 $ie.Document.getElementById("menuQuery").click();
 $ie.Document.getElementById("menuQuery").value = "DD-4266-CV";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
