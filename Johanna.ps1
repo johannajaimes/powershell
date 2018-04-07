6f1f430ea3ab37b8916a6f3532898b42ea9ad9d9
fragamus <innovative.engineer@gmail.com>
2018-04-06 17:07:18 -0700

    Update Johanna.ps1

--- Johanna.ps1
+++ Johanna.ps1
@@ -6,7 +6,7 @@ $ie.navigate("http://lmnapp75/BPMDDILP/");
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 $counter = 0;
 while (($counter -lt 100) -and ($ie.document -eq $null)) {Start-Sleep 1; $counter++}
-document.querySelectorAll('*');
+$ie.document.querySelectorAll('*');
 ($ie.document.getElementsByName("menuQuery")|select -first 1).click();
 ($ie.document.getElementsByName("menuQuery")|select -first 1).value = "DD-4266-CV";
 while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
